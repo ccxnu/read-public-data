eb25e701b6479ca25b97f88383ad43c1a7aaa551
Pablo Cuenca <108037606+ccxnu@users.noreply.github.com>
2024-05-29 15:35:42 -0500

    Update database configuration

--- src/infraestructure/database/ConnectionDB.ts
+++ src/infraestructure/database/ConnectionDB.ts
@@ -79,12 +79,12 @@ export class ConnectionDB {
     const connection = await this.getConnection();
     try {
       const [rows]: any[] = await connection.query(
-        `SELECT ip_address, creation_datetime FROM GeoData WHERE ip_address = ?`,
+        `SELECT ip, creation_datetime FROM GeoData WHERE ip = ?`,
         [data.ip],
       );
       if (rows.length === 0) {
         await connection.query(
-          `INSERT INTO GeoData (ip_address, country, countryCode, region, regionName, city, zip, latitud, longitud, timezone, isp, org, proveedor) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+          `INSERT INTO GeoData (ip, country, countryCode, region, regionName, city, zip, latitud, longitud, timezone, isp, org, proveedor) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
           [
             data.ip,
             data.country,
@@ -102,7 +102,7 @@ export class ConnectionDB {
           ],
         );
       } else {
-        console.log(`IP ${data.ip} already exists in the database.`);
+        console.log(`IP ${data.ip} already exists in the database.`); // Development
       }
     } finally {
       connection.release();
