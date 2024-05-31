d0682af1a004c4ee8c369336bbef3fbb60a4226c
Pablo Cuenca <108037606+ccxnu@users.noreply.github.com>
2024-05-31 16:38:16 -0500

    Update class soliticitarServicio

--- src/infraestructure/Common/Model/solicitarServicio.ts
+++ src/infraestructure/Common/Model/solicitarServicio.ts
@@ -1,10 +1,10 @@
-export class solicitarServicio {
+export class SolicitarServicio {
   metodo = 'POST';
   url_servicio: string;
   tipo_contenido = 'application/json';
-  key_autorizacion: string;
-  value_autorization: string;
-  header_adicionales: [{ value: string; key: string }];
+  key_autorizacion?: string;
+  value_autorizacion?: string;
+  header_adicionales?: { key: string; value: string }[] = [];
   esperar_respuesta = true;
-  data: any;
+  data?: any;
 }
